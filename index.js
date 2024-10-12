430c74eaaa4a8aa6a2fbb7a8521931fe7e0c4873
profrafamatos <rcmm@cesar.school>
2024-10-12 13:34:34 -0300

    Arquivos necessários para responder o exercício

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+  Implemente uma função chamada 'parOuImpar' que recebe um número inteiro numero) 
+  como parâmetro e verifica se um número é par ou ímpar. Se o número for par, deve 
+  retornar uma String com o texto "Par", se for ímpar deve retornar uma String com o texto "Ímpar", 
+  exatamente como escrito nesta descrição. 
+
+  Sintaxe:
+        if (condição) {
+        //  bloco de código que é executado se a condição for true
+        }else{
+        // bloco de código que é executado se a condição for false
+        }
+*/
+
+function parOuImpar(numero) {
+    /**** Escreva o código abaixo 👇******/
+    
+  
+    
+    /**** Escreva o código acima ☝️******/
+  }
+  
+  module.exports = parOuImpar; //Não altere essa linha
